e214b7fea040760719f8cdc1c7fff6fb4e197bf6
Weyman Leung <leungweyman@gmail.com>
2023-09-18 16:40:49 -0700

    change map ui, change listeners

--- frontend/src/components/Map/SpotMapIndex.js
+++ frontend/src/components/Map/SpotMapIndex.js
@@ -2,17 +2,20 @@ import { Wrapper } from '@googlemaps/react-wrapper';
 import { useEffect, useRef, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Map.css';
+import { useHistory } from 'react-router-dom';
 
 const SpotMapIndex = ({ spots, mapOptions } ) => { 
 	const [map, setMap] = useState(null);
 	const mapRef = useRef(null);
 	const markers = useRef({});
+	const history = useHistory();
 	let google = window.google;
 	let marker = window.marker;
 
 	useEffect(() => {
 		if(!map) { 
-			const defaultOptions = { zoom: 8, center: {lat: 37.64654, lng: -121.39977}};
+			const defaultOptions = { zoom: 10, center: {lat: 37.8253, lng: -122.3700}, mapTypeId: google.maps.MapTypeId.ROADMAP,
+				mapTypeControl: false, fullscreenControl: false,  disableDefaultUI: true};
 			const newMap = new google.maps.Map(mapRef.current, {...defaultOptions, ...mapOptions});
 			setMap(newMap);
 		}
@@ -31,15 +34,25 @@ const SpotMapIndex = ({ spots, mapOptions } ) => {
 					markers.current[spot?.id] = marker;
 
 					marker.addListener('click', () => {
+						map.panTo(markers.current[spot?.id].getPosition());
+						history.push(`/spots/${spot.id}`);
+					});
+
+					marker.addListener('mouseover', () => {
 						if (windowOpened) {
 							infowindow.close();
 						}
-						// const infoContent = `<a href="/spots/${spot?.id}"}> ${spot?.name} </a>`;
 						infowindow.setContent(spot?.name);
-						map.panTo(markers.current[spot?.id].getPosition());
 						infowindow.open(map, markers.current[spot?.id]);
 						windowOpened = true;
 					});
+
+					marker.addListener('mouseout', () => {
+						if (windowOpened) {
+							infowindow.close();
+						}
+						windowOpened = false;
+					})
 				}
 			})
 		}
